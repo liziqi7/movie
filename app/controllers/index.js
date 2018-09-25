ee7060f5dd2738a6790dae5eb7d76da844dca167
liziqi7 <liziqi7@qq.com>
2018-09-25 23:39:27 +0800

    提交

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,25 +15,42 @@ exports.index = function(req, res) {
 // search page
 exports.search = function(req, res) {
     var catId = req.query.cat;
-    var page = Number(req.query.p);
+    var q = req.query.q;
+    var page = Number(req.query.p)||0;
     var count = 1;
     var index = page * count;
-    console.log();
-    Catetory.find({ _id: catId }).populate({ path: 'movies', potions: { limit: 1, skip: index } }).exec(function(err, catetories) {
-        if (err) console.log(err)
-        console.log('------------------------')
-        console.log('catetories', catetories);
-        var catetory = catetories[0] || {};
-        var movies = catetory.movies || [];
-        var results = movies.slice(index, index + count);
-        res.render('results', { // 渲染index 首页
-            title: 'imooc 结果列表页面',
-            keyword: catetory.name,
-            currentPage: page + 1,
-            catId:catId,
-            totalPage: Math.ceil(movies.length / count),
-            movies: results
-        });
-    })
-
+    if (catId) {
+        Catetory.find({ _id: catId }).populate({ path: 'movies', potions: { limit: 1, skip: index } }).exec(function(err, catetories) {
+            if (err) console.log(err)
+            console.log('------------------------')
+            console.log('catetories', catetories);
+            var catetory = catetories[0] || {};
+            var movies = catetory.movies || [];
+            var results = movies.slice(index, index + count);
+            res.render('results', { // 渲染index 首页
+                title: 'imooc 结果列表页面',
+                keyword: catetory.name,
+                currentPage: page + 1,
+                query: 'cat=' + catId,
+                totalPage: Math.ceil(movies.length / count),
+                movies: results
+            });
+        })
+    } else {
+        Movie.find({ title: new RegExp(q+'.*','i') }).exec(function(err, movies) {
+            if (err) console.log(err)
+            console.log('------------------------')
+            console.log('movies', movies);
+            var results = movies.slice(index, index + count);
+            // console.log('results', results);
+            res.render('results', { // 渲染index 首页
+                title: 'imooc 结果列表页面',
+                keyword: q,
+                currentPage: page + 1,
+                query: 'q=' + q,
+                totalPage: Math.ceil(movies.length / count),
+                movies: results
+            });
+        })
+    }
 }
